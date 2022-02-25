4c4a7790f5fadc3ae8e89b4682bfb07c73c59738
danibarker <caseybarker86@gmail.com>
2022-02-25 00:56:05 -0700

    init commit

---
+++ 1_getting_started/makingCoffee.js
@@ -0,0 +1,13 @@
+console.log("start");
+console.log("do I have coffee beans? (no)");
+console.log("add coffee beans to shopping list");
+console.log("buy coffee beans");
+console.log("grind beans");
+console.log("is the coffee pot clean? (no)");
+console.log("clean the pot");
+console.log("add filter to coffee machine");
+console.log("add coffee grounds to filter");
+console.log("add water to coffee machine");
+console.log("start coffee maker");
+console.log("wait for coffee to finish brewing");
+console.log("end");
